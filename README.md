ab2d2c79a5b6d142241d17962010f6d4b8beaf1e
sachin08sachin <sgsachin359@gmail.com>
2022-09-29 00:44:53 +0530

    HomeScreen-ListProducts

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # E-commerce_Amazon
 
-1. => 1.create folder frontend
-      2.create Navbar(install npx create-react-app frontend)
+##SECTION 1: ##Introduction
+           <p>Amazon_clone </p>
+##SECTION 2: ##<h1>React</h1>
+             1. create folder frontend
+             2. create Navbar(install npx create-react-app frontend) and Listproducts
+##SECTION 3: ##<h1>Home Screen</h1>
+             List Products
+             <h2>
+             1. create Products array
+             2. add product images
+             3. render images
+             4. style products </h2>
+             Note: ##<p>A "key" is a special string attribute you need to include creating list of elements
+             key give the element a stable identity
+             keys help React which item changed, added or deleted
+             Help in efficient update of user interface
+             Here {product.slug} is unique so use that </p>
+              
+            
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,32 @@
-
+import data from './data';
 
 function App() {
   return (
-    <div >
-      <header >
-       <a href="/">amazon</a>
+    <div>
+      <header>
+        <a href="/">amazon</a>
       </header>
       <main>
-        List Products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+              </a>
+              
+              <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+              <p>{product.name}</p>
+              </a>
+              <p><strong>${product.price}</strong></p>
+              <button>Add to Cart</button>
+              </div>
+             
+            </div>
+          ))}
+        </div>
+        {/* render products from data */}
       </main>
     </div>
   );
