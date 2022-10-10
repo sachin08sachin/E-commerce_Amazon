fd8eabaf002b48e30aff25087fd786961a78bd18
sachin08sachin <sgsachin359@gmail.com>
2022-10-10 17:26:14 +0530

    products in product screen

--- README.md
+++ README.md
@@ -17,15 +17,43 @@
              keys help React which item changed, added or deleted
              Help in efficient update of user interface
              Here {product.slug} is unique so use that </p>
-
-             ##Add_routing
-             <h2>
-             1. npm i react-router-dom
-             2. create Route for HomeScreen
-             3. create Router fo ProductScreen
-              </h2>
-              ##Note: 1.useParams is a hook from react-router-dom . It helps in get the slug from URL and show it in ProductScreen.
+ ##Add_routing
+    1. npm i react-router-dom
+    2. create Route for HomeScreen
+    3. create Router fo ProductScreen           
+           
+  ##Note: 1.useParams is a hook from react-router-dom . It helps in get the slug from URL and show it in ProductScreen.
                       2. Remove anchor tag and replace with <link>
                          -Link is a component from react-router-dom 
-                         -There is no page refresh whle using <link to="/"> </Link>
-            
+                         -There is no page refresh whle using <link to="/"> </Link>             
+             
+   ##Create Node.JS Server
+                    1. run npm init in root folder
+                    2. Update package.json set type: module
+                       type => module because we are going to use import instead of require for importing packages(using ES6)
+                    3. Add .js to imports
+                    4. npm install express
+                    5. create server.js
+                    6. add start command as node backend/server.js
+                    7. require express
+                    8. create route for / return backend is ready.
+                    9. move products.js from frontend to backend
+                   10. create route for /api/products
+                   11. return products
+                   12. run npm start
+                      (here we install "npm install nodemon --save-dev" because this package shouldn't be shift to
+                      the production its just for development, when development is done am not going to use it anymore)
+                      In package.json add 
+                                     "scripts": {  "start": "nodemon server.js" }
+                                     to start nodemon.js automatically ,Now we use command "npm start" to get result.         
+          ## Fetch Products From Backend
+                   1. set proxy in package.json
+                   2. npm install axios
+                   3. use state hook
+                   4. use effect hook
+                   5. use reducer hook  
+#usereducer - replace useState with usereducer to manage complex state in HomeScreen Components.
+           ##SECTION_4 : REACT_BOOTSTRAP
+                      1.Add Bootstrap.                   
+                                                    
+              
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,73 +1,68 @@
 // import { Link } from 'react-router-dom';
 import { useEffect, useReducer } from 'react';
 import axios from 'axios';
-import logger from 'use-reducer-logger'
+import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../component/Product.js';
-
+import { Helmet } from 'react-helmet-async';
 
 const reducer = (state, action) => {
-      switch(action.type) {
-        case 'FETCH_REQUEST' :
-          return {...state, loading: true };
-        case 'FETCH_SUCCESS' :
-          return {...state, products: action.payload , loading:false};
-        case 'FETCH_FAILURE' :
-           return { ...state, loading:false , error: action.payload };
-        default:
-          return state;
-      }
-}
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAILURE':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
-
-
-
-
-function HomeScreen(){
-  const [{products,loading,error},dispatch]=useReducer(logger(reducer), {
+function HomeScreen() {
+  const [{ products, loading, error }, dispatch] = useReducer(logger(reducer), {
     products: [],
-    loading:'true',
-    error:'',
+    loading: 'true',
+    error: '',
   });
   // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      dispatch({type: 'FETCH_REQUEST'});
-      try{
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
         const result = await axios.get('/api/products');
-        dispatch({type:'FETCH_SUCCESS', payload: result.data});
-        
-      }catch(err){
-         dispatch({type:'FETCH_FAILURE', payload: err.message});
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAILURE', payload: err.message });
       }
-      
+
       // setProducts(result.data);
     };
     fetchData();
   }, []);
-    return (
-      <div>
-          <h1>Featured Products</h1>
-          <div className="products">
-            {
-            loading? (<div>Loading...</div>)
-            :
-            error? (<div>{error}</div>)
-            :
-            (
-              <Row>
-             { products.map((product) => (
-           
-                <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
-              <Product product={product}></Product>
+  return (
+    <div>
+       <Helmet>
+        <title>Amazona</title>
+      </Helmet>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
               </Col>
-            ))
-            }
-            </Row>
-          )}
-          </div>
+            ))}
+          </Row>
+        )}
+      </div>
     </div>
-    ) 
+  );
 }
 export default HomeScreen;
