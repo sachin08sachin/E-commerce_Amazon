55bf11e5faf9f117b1f2977f1c18b7146dbe54c4
sachin08sachin <sgsachin359@gmail.com>
2022-10-11 19:08:57 +0530

    create React Context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,49 @@
-
 import HomeScreen from './screens/HomeScreen';
-import { BrowserRouter,  Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
+import { useContext } from 'react';
+import { Store } from './Store';
+import { Link } from 'react-router-dom';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <BrowserRouter>
-    {/* <div> */}
-      <div  className="d-flex flex-column site-container">
+      {/* <div> */}
+      <div className="d-flex flex-column site-container">
         <header>
-        <Navbar bg="dark" variant="dark">
+          <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>amazona</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
-       
         </header>
         <main>
-        <Container className="mt-3">
+          <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
-        
         </main>
         <footer>
           <div className="text-center">All rights reserved</div>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useReducer } from 'react';
+import { useContext, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -11,58 +11,64 @@ import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
 import Rating from '../component/Rating';
 
-
 import { getError } from '../utils';
 import LoadingBox from '../component/LoadingBox';
 import MessageBox from '../component/MessageBox';
+import { Store } from '../Store';
 //  <---------------------Get the slug from URL to show it in screen, to do that we need to use Hook(UseParams()) from react-router-dom----------------->
 
-const reducer = (state,action) => {
-  switch(action.type) {
+const reducer = (state, action) => {
+  switch (action.type) {
     case 'FETCH_REQUEST':
-      return {...state , loading:true};
+      return { ...state, loading: true };
     case 'FETCH_SUCCESS':
-      return {...state, loading:false, product:action.payload};
+      return { ...state, loading: false, product: action.payload };
     case 'FETCH_FAILURE':
-      return {...state, loading:false , error:action.payload};
+      return { ...state, loading: false, error: action.payload };
     default:
       return state;
   }
-}
-
+};
 
 function ProductScreen() {
-  
   const params = useParams();
-  const {slug} = params;
+  const { slug } = params;
 
-  const [{product,loading,error},dispatch]=useReducer(reducer,{
-    product:[],
-    loading:true,
-    error:'',
-   
+  const [{ product, loading, error }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: '',
   });
-  useEffect( () => {
-    const fetchData = async () =>
-    {dispatch ({type:'FETCH_REQUEST'})
-    try{
-      const result =await axios.get(`/api/products/slug/${slug}`) 
-      dispatch({type:'FETCH_SUCCESS', payload:result.data})
-    }catch(err){
-       dispatch({type:'FETCH_FAILURE', payload: getError(err)})
-    }
-  };
-  fetchData();
-  },[slug])
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAILURE', payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, [slug]);
 
-  return loading? (
-    <div>  <LoadingBox />    </div>
-  ) : error? (
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const addToCartHandler = () => {
+    ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...state, quantity: 1 } });
+  };
+
+  return loading ? (
+    <div>
+      {' '}
+      <LoadingBox />{' '}
+    </div>
+  ) : error ? (
     <div>
       <MessageBox variant="danger">{error}</MessageBox>
     </div>
-  ) : ( <div>
-    <Row>
+  ) : (
+    <div>
+      <Row>
         <Col md={6}>
           <img
             className="img-large"
@@ -117,7 +123,9 @@ function ProductScreen() {
                 {product.countInStock > 0 && (
                   <ListGroup.Item>
                     <div className="d-grid">
-                      <Button variant="primary">Add to Cart</Button>
+                      <Button onClick={addToCartHandler} variant="primary">
+                        Add to Cart
+                      </Button>
                     </div>
                   </ListGroup.Item>
                 )}
@@ -127,6 +135,6 @@ function ProductScreen() {
         </Col>
       </Row>
     </div>
-  )
-};
+  );
+}
 export default ProductScreen;
