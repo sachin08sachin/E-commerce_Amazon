d8851351309190d3247c454da4234253e9f41ec4
sachin08sachin <sgsachin359@gmail.com>
2022-10-12 20:28:14 +0530

    cart screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useContext, useEffect, useReducer } from 'react';
-import { useParams } from 'react-router-dom';
+import {  useNavigate, useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
@@ -31,6 +31,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+  const navigate= useNavigate();
   const params = useParams();
   const { slug } = params;
 
@@ -68,6 +69,7 @@ function ProductScreen() {
       
       payload: { ...product, quantity },
     });
+    navigate('/cart');
   };
 
   return loading ? (
