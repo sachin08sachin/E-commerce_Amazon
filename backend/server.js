af6d5bfea47f8207f948f0b0425f27505a1a5ce8
sachin08sachin <sgsachin359@gmail.com>
2022-10-12 09:22:58 +0530

    add quantity

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,15 @@ app.get('/api/products/slug/:slug', (req,res) => {
         res.status(404).send({message : 'PRODUCT NOT FOUND'});
     }
 });
+app.get('/api/products/:id' , (req,res) => {
+    const product = data.products.find( (x) => x._id === req.params.id);
+    if(product){
+        res.send(product);
+    }
+    else{
+        res.status(404).send({message : 'PRODUCT NOT FOUND'});
+    }
+})
 
 const port = process.env.PORT || 5000;
 
